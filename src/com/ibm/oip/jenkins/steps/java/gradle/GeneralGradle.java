1e96b58494677ce9c293f6a8d15a8107cd6137ce
tobilarscheid <tobilarscheid@gmail.com>
2017-03-08 14:56:42 +0100

    serializable

--- src/com/ibm/oip/jenkins/steps/java/gradle/GeneralGradle.java
+++ src/com/ibm/oip/jenkins/steps/java/gradle/GeneralGradle.java
@@ -4,7 +4,9 @@ import com.ibm.oip.jenkins.steps.Step;
 import com.ibm.oip.jenkins.steps.java.gradle.release.FinishRelease;
 import com.ibm.oip.jenkins.steps.java.gradle.release.PrepareRelease;
 
-public class GeneralGradle {
+import java.io.Serializable;
+
+public class GeneralGradle implements Serializable{
     public static Step ASSEMBLE = new Assemble();
     public static Step PREPARE_RELEASE = new PrepareRelease();
     public static Step UNIT_TEST = new UnitTest();
