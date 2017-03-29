294ae503657b60ae1279560281abae145e41086f
Jan Schmitz-Hermes <jan@schmitz-hermes.de>
2017-03-29 13:42:29 +0200

    fix again

--- src/com/ibm/oip/jenkins/Pipeline.groovy
+++ src/com/ibm/oip/jenkins/Pipeline.groovy
@@ -14,7 +14,7 @@ class Pipeline implements Serializable {
             buildContext.getScriptEngine().ws(buildContext.getProject() + "-workspace") {
                 buildContext.getScriptEngine().configFileProvider(
                         [buildContext.getScriptEngine().configFile(fileId: "${buildContext.getGroup()}-config", variable: 'PROJECT_CONFIG')]) {
-                    def variables = load env.PROJECT_CONFIG
+                    def variables = load buildContext.getScriptEngine().env.PROJECT_CONFIG
                     withEnv(variables) {
                         buildContext.getScriptEngine().deleteDir();
                         for (int i = 0; i < this.steps.size(); i++) {
