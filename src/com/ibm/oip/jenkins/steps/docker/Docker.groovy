435e896ff1c42314f2c1ecee17ff270457d1342c
Jan Schmitz-Hermes <jan@schmitz-hermes.de>
2017-03-29 14:29:51 +0200

    add check docker build

--- src/com/ibm/oip/jenkins/steps/docker/Docker.groovy
+++ src/com/ibm/oip/jenkins/steps/docker/Docker.groovy
@@ -4,4 +4,5 @@ import com.ibm.oip.jenkins.steps.Step
 
 class Docker {
     public static Step CREATE_AND_PUSH_CONTAINER = new CreatePushContainer();
+    public static Step CHECK_DOCKER_BUILD = new CheckDockerBuild();
 }
