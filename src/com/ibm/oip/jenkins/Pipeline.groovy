803d95b4e56caf5a82f92bf09a11d77fdfea5bf4
Jan Schmitz-Hermes <jan@schmitz-hermes.de>
2017-03-29 13:35:11 +0200

    Add configuration mechanism

--- src/com/ibm/oip/jenkins/Pipeline.groovy
+++ src/com/ibm/oip/jenkins/Pipeline.groovy
@@ -3,7 +3,7 @@ package com.ibm.oip.jenkins
 class Pipeline implements Serializable {
     def steps;
 
-    def pattern = ~/^.*$/; 
+    def pattern = ~/^.*$/;
 
     public boolean canBuild(String branch) {
         return pattern.matcher(branch).matches();
@@ -12,9 +12,15 @@ class Pipeline implements Serializable {
     public void run(buildContext) {
         buildContext.getScriptEngine().node {
             buildContext.getScriptEngine().ws(buildContext.getProject() + "-workspace") {
-                buildContext.getScriptEngine().deleteDir();
-                for(int i = 0; i < this.steps.size(); i++) {
-                    this.steps[i].doStep(buildContext);
+                buildContext.getScriptEngine().configFileProvider(
+                        [configFile(fileId: "${buildContext.getGroup()}-config", variable: 'PROJECT_CONFIG')]) {
+                    def variables = load env.PROJECT_CONFIG
+                    withEnv(variables) {
+                        buildContext.getScriptEngine().deleteDir();
+                        for (int i = 0; i < this.steps.size(); i++) {
+                            this.steps[i].doStep(buildContext);
+                        }
+                    }
                 }
             }
         }
