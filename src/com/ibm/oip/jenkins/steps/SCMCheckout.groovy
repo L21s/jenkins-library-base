2fed06631bd4ef8465e676cd5d8a886c41aa66bb
Jan Schmitz-Hermes <jan@schmitz-hermes.de>
2017-03-30 11:04:29 +0200

    remove noncps

--- src/com/ibm/oip/jenkins/steps/SCMCheckout.groovy
+++ src/com/ibm/oip/jenkins/steps/SCMCheckout.groovy
@@ -3,7 +3,6 @@ package com.ibm.oip.jenkins.steps;
 import com.ibm.oip.jenkins.BuildContext;
 
 class SCMCheckout implements Step {
-    @NonCPS
     public void doStep(BuildContext buildContext) {
         buildContext.changeStage("Checkout");
         buildContext.getScriptEngine().checkout buildContext.getScriptEngine().scm
