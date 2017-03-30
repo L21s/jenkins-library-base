202f05a38a04622d875d30523927e64fbf0e4b6a
Jan Schmitz-Hermes <jan@schmitz-hermes.de>
2017-03-30 09:49:53 +0200

    regex for gorup / project

--- src/com/ibm/oip/jenkins/steps/SCMCheckout.groovy
+++ src/com/ibm/oip/jenkins/steps/SCMCheckout.groovy
@@ -3,11 +3,17 @@ package com.ibm.oip.jenkins.steps;
 import com.ibm.oip.jenkins.BuildContext;
 
 class SCMCheckout implements Step {
+    @NonCPS
     public void doStep(BuildContext buildContext) {
         buildContext.changeStage("Checkout");
         buildContext.getScriptEngine().checkout buildContext.getScriptEngine().scm
-        // buildContext.getScriptEngine().sh "git config --global user.email 'jenkins@${buildContext.devstack.getBaseUrl()}'"
-        //buildContext.getScriptEngine().sh 'git config --global user.name "jenkins"'
+
         buildContext.addGitProperties();
+
+        // retrieve org and project
+        def url = buildContext.getScriptEngine().scm.getUserRemoteConfigs()[0].getUrl()
+        def parts = url =~ ".*\\/(.*?)\\/(.*?)\\.git"
+        buildContext.setGroup(parts[0][1])
+        buildContext.setProject(parts[0][2])
     }
 }
