9b88ad0eb812f327bf7f87ce391c8a8dfff00f90
Jan Schmitz-Hermes <jan@schmitz-hermes.de>
2017-03-31 15:22:06 +0200

    fix

--- src/com/ibm/oip/jenkins/steps/java/gradle/release/PrepareRelease.groovy
+++ src/com/ibm/oip/jenkins/steps/java/gradle/release/PrepareRelease.groovy
@@ -29,7 +29,7 @@ class PrepareRelease extends AbstractGradleStep {
 
         def bump = "patch";
         buildContext.getScriptEngine() withCredentials([[$class: 'StringBinding', credentialsId: "${buildContext.getGroup()}-sonarqube-github-reporter", variable: 'GITHUB_OAUTH_TOKEN']])  {
-            def labels = buildContext.getScriptEngine().sh(returnStdout: true, script: "curl -X GET -H 'Authorization: token ${buildContext.getScriptEngine().env.GITHUB_OAUTH_TOKEN}' \$GITHUB_API_URL/repos/${buildContext.getGroup()}/${buildContext.getProject()}/issues/${prNumber}/labels | jq -r '[.[].name]'")
+            def labels = buildContext.getScriptEngine().sh(returnStdout: true, script: "curl -X GET -H 'Authorization: token ${buildContext.getScriptEngine().env.GITHUB_OAUTH_TOKEN}' \$GITHUB_API_URL/repos/${buildContext.getGroup()}/${buildContext.getProject()}/issues/${prNumber}/labels | jq -r '.[].name'").split('\\n').collect {it as String}
             buildContext.getScriptEngine().sh "echo ${labels}"
             labels.each { label ->
                 buildContext.getScriptEngine().sh "echo ${label}"
