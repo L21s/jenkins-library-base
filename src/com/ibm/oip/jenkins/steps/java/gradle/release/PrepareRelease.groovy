b6cc6e2fb5312dc8f7f1be50c0b12f8715a4cda7
Jan Schmitz-Hermes <jan@schmitz-hermes.de>
2017-03-31 12:28:47 +0200

    change env

--- src/com/ibm/oip/jenkins/steps/java/gradle/release/PrepareRelease.groovy
+++ src/com/ibm/oip/jenkins/steps/java/gradle/release/PrepareRelease.groovy
@@ -26,7 +26,7 @@ class PrepareRelease extends AbstractGradleStep {
         def prNumber = pr[0][1];
 
         buildContext.getScriptEngine() withCredentials([[$class: 'StringBinding', credentialsId: "${buildContext.getGroup()}-sonarqube-github-reporter", variable: 'GITHUB_OAUTH_TOKEN']]) {
-            buildContext.getScriptEngine().sh "curl -i -H 'Authorization: token \$GITHUB_OAUTH_TOKEN' \$GITHUB_API_URL/repos/${buildContext.getGroup()}/${buildContext.getProject()}/issues/${prNumber}/labels | jq -r \".[].name\" > labels.txt"
+            buildContext.getScriptEngine().sh "curl -i -H 'Authorization: token ${buildContext.getScriptEngine().env.GITHUB_OAUTH_TOKEN}' \$GITHUB_API_URL/repos/${buildContext.getGroup()}/${buildContext.getProject()}/issues/${prNumber}/labels | jq -r \".[].name\" > labels.txt"
 
             String[] labels = new File('labels.txt')
             def version = "patch";
