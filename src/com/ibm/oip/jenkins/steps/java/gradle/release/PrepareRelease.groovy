93017019dc078982f41b200a8d49b678f71bc3a2
Jan Schmitz-Hermes <jan@schmitz-hermes.de>
2017-04-02 22:34:37 +0200

    change check for group

--- src/com/ibm/oip/jenkins/steps/java/gradle/release/PrepareRelease.groovy
+++ src/com/ibm/oip/jenkins/steps/java/gradle/release/PrepareRelease.groovy
@@ -24,7 +24,7 @@ class PrepareRelease extends AbstractGradleStep {
     public String determineVersionDump() {
         def prNumber = retrievePrId(buildContext.getCommitMessage())
         if (!prNumber) {
-            return "Patch"
+            return "patch"
         }
 
         def bump = "patch";
