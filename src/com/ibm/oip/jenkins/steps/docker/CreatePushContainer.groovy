9b8dd655d75a5a86ab79b0d24a3251c955ad2bec
Jan Schmitz-Hermes <jan@schmitz-hermes.de>
2017-04-04 09:38:29 +0200

    adapt push container

--- src/com/ibm/oip/jenkins/steps/docker/CreatePushContainer.groovy
+++ src/com/ibm/oip/jenkins/steps/docker/CreatePushContainer.groovy
@@ -10,10 +10,18 @@ class CreatePushContainer implements Step {
         def project = buildContext.getProject();
         def version = buildContext.getVersion();
         buildContext.getScriptEngine().sh "docker build -t \$DOCKER_REGISTRY_URL/${project}:${version} -t \$DOCKER_REGISTRY_URL/${project}:latest .";
-        buildContext.changeStage('Push Container');
-        buildContext.getScriptEngine().sh "docker push \$DOCKER_REGISTRY_URL/${project}:${version}";
-        buildContext.getScriptEngine().sh "docker push \$DOCKER_REGISTRY_URL/${project}:latest";
-        buildContext.getScriptEngine().sh "docker rmi \$DOCKER_REGISTRY_URL/${project}:${version}";
-        buildContext.getScriptEngine().sh "docker rmi \$DOCKER_REGISTRY_URL/${project}:latest";
+
+        buildContext.getScriptEngine().withCredentials([
+                [$class: 'UsernamePasswordMultiBinding', credentialsId: "${buildContext.getGroup()}-cloudfoundry", usernameVariable: 'CLOUD_FOUNDRY_USERNAME', passwordVariable: 'CLOUD_FOUNDRY_PASSWORD']]) {
+            buildContext.changeStage('Push Container');
+            buildContext.getScriptEngine().sh "cf login -u ${buildContext.getScriptEngine().env.CLOUD_FOUNDRY_USERNAME} -p ${buildContext.getScriptEngine().env.CLOUD_FOUNDRY_PASSWORD} -a \$BLUEMIX_API_URL"
+            buildContext.getScriptEngine().sh "cf ic login"
+            buildContext.getScriptEngine().sh "docker push \$DOCKER_REGISTRY_URL/${project}:${version}";
+            buildContext.getScriptEngine().sh "docker push \$DOCKER_REGISTRY_URL/${project}:latest";
+
+            // delete the images from jenkins
+            buildContext.getScriptEngine().sh "docker rmi \$DOCKER_REGISTRY_URL/${project}:${version}";
+            buildContext.getScriptEngine().sh "docker rmi \$DOCKER_REGISTRY_URL/${project}:latest";
+        }
     }
 }
