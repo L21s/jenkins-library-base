fda790cc756fe211bb066c009d6df9f4788a8843
Jan Schmitz-Hermes <jan@schmitz-hermes.de>
2017-04-06 10:16:49 +0200

    Add sonarqube coverage parameters

--- src/com/ibm/oip/jenkins/steps/java/gradle/analysis/StaticAnalysis.groovy
+++ src/com/ibm/oip/jenkins/steps/java/gradle/analysis/StaticAnalysis.groovy
@@ -15,11 +15,13 @@ class StaticAnalysis extends AbstractGradleStep {
 
         buildContext.getScriptEngine().withCredentials([
                             [$class: 'UsernamePasswordMultiBinding', credentialsId: "${buildContext.getGroup()}-sonarqube", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-            doGradleStep(buildContext, "sonarqube -Dsonar.branch=${buildContext.branch} " +
+            doGradleStep(buildContext, "sonarqube " +
                     "-Dsonar.buildbreaker.skip=$skip " +
                     "-Dsonar.host.url=\$SONARQUBE_URL " +
                     "-Dsonar.login='${buildContext.getScriptEngine().env.USERNAME}' " +
-                    "-Dsonar.password='${buildContext.getScriptEngine().env.PASSWORD}' ");
+                    "-Dsonar.password='${buildContext.getScriptEngine().env.PASSWORD}' " +
+                    "-Dsonar.jacoco.itReportPath=build/jacoco/integrationTest.exec -Dsonar.jacoco.reportPath=build/jacoco/test.exec");
+            // The last line should acutally not be needed since SQ 6.2 - but without it does not recognize the coverage
         }
     }
 }
