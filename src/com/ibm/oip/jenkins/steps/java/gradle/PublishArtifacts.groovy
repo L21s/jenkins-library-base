0ccc831e5b4ee80f739998225342fc44165a84f5
tobilarscheid <tobilarscheid@gmail.com>
2017-05-16 14:23:46 +0200

    fix typo

--- src/com/ibm/oip/jenkins/steps/java/gradle/PublishArtifacts.groovy
+++ src/com/ibm/oip/jenkins/steps/java/gradle/PublishArtifacts.groovy
@@ -1,12 +1,11 @@
-package com.ibm.oip.jenkins.steps.java.gradle;
+package com.ibm.oip.jenkins.steps.java.gradle
 
-import com.ibm.oip.jenkins.BuildContext;
-import com.ibm.oip.jenkins.steps.Step;
+import com.ibm.oip.jenkins.BuildContext
 
 class PublishArtifacts extends AbstractGradleStep {
     @Override
     public void doStep(BuildContext buildContext) {
-        buildContext.changeStage("Publish artifacts>>")
+        buildContext.changeStage("Publish artifacts")
         doGradleStep(buildContext, "publish");
     }
 }
