6b942221b61eb9bd8e114833daa11bce9de932a0
Jan Schmitz-Hermes <jan@schmitz-hermes.de>
2018-01-24 14:57:44 +0100

    check out to local branch

--- src/com/ibm/oip/jenkins/steps/SCMCheckout.groovy
+++ src/com/ibm/oip/jenkins/steps/SCMCheckout.groovy
@@ -8,7 +8,7 @@ class SCMCheckout implements Step {
         buildContext.getScriptEngine().checkout([
                 $class: 'GitSCM',
                 branches: buildContext.getScriptEngine().scm.branches,
-                extensions: [[$class: 'LocalBranch', localBranch: 'master'], [$class: 'CloneOption', depth: 0, noTags: false, reference: '', shallow: false]],
+                extensions: [[$class: 'LocalBranch', localBranch: buildContext.branch], [$class: 'CloneOption', depth: 0, noTags: false, reference: '', shallow: false]],
                 userRemoteConfigs: buildContext.getScriptEngine().scm.userRemoteConfigs
         ])
 
