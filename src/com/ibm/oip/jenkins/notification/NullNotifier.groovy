09fc3d957c5191c2efd8d86edc17122f5b556bc5
Jan Schmitz-Hermes <jan@schmitz-hermes.de>
2018-02-19 10:54:39 +0100

    remove debug statements

--- src/com/ibm/oip/jenkins/notification/NullNotifier.groovy
+++ src/com/ibm/oip/jenkins/notification/NullNotifier.groovy
@@ -5,7 +5,6 @@ import com.ibm.oip.jenkins.BuildContext
 class NullNotifier implements  Notifier, Serializable {
     @Override
     void notifyFailure(BuildContext buildContext) {
-        buildContext.getScriptEngine().println "notifiy"
         // nothing to do
     }
 }
