e9e5d07fd4dd5e59df62f66396cb7fca996d3d44
Jan Schmitz-Hermes <jan@schmitz-hermes.de>
2018-02-19 10:48:37 +0100

    debug

--- src/com/ibm/oip/jenkins/notification/NullNotifier.groovy
+++ src/com/ibm/oip/jenkins/notification/NullNotifier.groovy
@@ -5,6 +5,7 @@ import com.ibm.oip.jenkins.BuildContext
 class NullNotifier implements  Notifier {
     @Override
     void notifyFailure(BuildContext buildContext) {
+        buildContext.getScriptEngine().println "notifiy"
         // nothing to do
     }
 }
